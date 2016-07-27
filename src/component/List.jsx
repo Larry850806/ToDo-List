19958e2be4d9c7214a105ee249133494e222e363
Larry <pudding850806@gmail.com>
2016-07-27 22:48:11 +0800

    add badge and build

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Immutable from 'immutable';
+import Badge from 'material-ui/lib/badge';
 import Item from './Item';
 
 var List = React.createClass({
@@ -10,9 +11,15 @@ var List = React.createClass({
         return false;
     },
     render(){
+        var badgeVisibility = this.props.items.size == 0 ? 'hidden' : 'visible';
         return (
             <div>
-                <font size="10"> {this.props.name} </font>
+                <div style={{position: 'relative', left: '25px'}}>
+                    <font size="10"> {this.props.name} </font>
+                    <span style={{position: 'relative', left: '-30px', top: '-15px', visibility: badgeVisibility}}>
+                        <Badge badgeContent={this.props.items.size} secondary={true}></Badge>
+                    </span>
+                </div>
                 {
                     this.props.items.map(function(item, i){
                         return <Item key={i} content={item} onClick={() => this.props.onClick(i)} />
